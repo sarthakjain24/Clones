55faeecb43300bc706663ac2607c83f392fa1cb5
sarthakjain24 <sarthakmanutd24@gmail.com>
2022-08-08 10:29:32 -0600

    Created the main page

--- Uber/App.js
+++ Uber/App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+
 
 export default function App() {
   return (
-    //Alternate to div, and style names instead of class names
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Provider store={store}>
+    <HomeScreen />
+    {/* <View style={styles.container}>
+      <Text>Hi, this is us making Uber!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> */}
+    </Provider>
   );
 }
 
