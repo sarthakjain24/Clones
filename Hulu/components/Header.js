bd943b72df8f849a8976a7066fbae1e608b4ab84
sarthakjain24 <sarthakmanutd24@gmail.com>
2022-08-16 10:55:03 -0600

    Added home button

--- Hulu/components/Header.js
+++ Hulu/components/Header.js
@@ -1,10 +1,25 @@
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon,
+} from "@heroicons/react/outline"
+
 function Header() {
     return (
         <header className="">
-            <h1>This is the header</h1>
+            <div>
+                <HeaderItem title='HOME' Icon={HomeIcon}
+                />
+            </div>
             {/* Lazy loaded by default */}
-            <Image src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
+            <Image
+                className="object-contain"
+                src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
                 width={200}
                 height={100} />
         </header>
