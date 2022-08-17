5437df4380b5aac3a38932f41733213e54a982f0
sarthakjain24 <sarthakmanutd24@gmail.com>
2022-08-17 17:17:26 -0600

    Integrated the database

--- Hulu/pages/index.js
+++ Hulu/pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
       <Head>
@@ -11,12 +14,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/*Header*/}
       <Header
 
       />
-      {/*Nav*/}
-      {/*Results*/}
+      <Nav />
+      <Results />
     </div>
   )
+}
+
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`
+
+  ).then(res => res.json());
+  return {
+    props: {
+      results: requests.results,
+    }
+  }
 }
