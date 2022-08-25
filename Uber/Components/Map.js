b63cb526153e263d0287f81aca05c472bbe561cf
sarthakjain24 <sarthakmanutd24@gmail.com>
2022-08-25 14:37:14 -0600

    Need to implement the Directions API

--- Uber/Components/Map.js
+++ Uber/Components/Map.js
@@ -1,13 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import MapView, { Marker } from "react-native-maps"
 import tw from "tailwind-react-native-classnames"
 import { useSelector } from 'react-redux'
-import { selectOrigin } from '../slices/navSlice'
+import MapViewDirections from 'react-native-maps-directions'
+import { selectDestination, selectOrigin } from '../slices/navSlice'
+import { GOOGLE_MAPS_APIKEY } from "@env";
 const Map = () => {
   const origin = useSelector(selectOrigin)
+  const destination = useSelector(selectDestination)
+  const mapRef = useRef(null);
+  useEffect(() => {
+    if (!origin || !destination) return;
+    //Zoom and fit to markers
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    })
+  }, [origin, destination])
   return (
     <MapView style={tw`flex-1`}
+      ref={mapRef}
       initialRegion={
         {
           latitude: origin.location.lat,
@@ -17,6 +29,15 @@ const Map = () => {
         }
       }
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="black"
+        />
+      )}
       {origin?.location && (
         <Marker
           coordinate={{
@@ -27,6 +48,19 @@ const Map = () => {
           title="Origin"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
         />
       )}
 
