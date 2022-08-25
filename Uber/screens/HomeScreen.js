b203588dae7cf437ecd9739c45e651384479870a
sarthakjain24 <sarthakmanutd24@gmail.com>
2022-08-25 13:09:19 -0600

    Getting all the location now

--- Uber/screens/HomeScreen.js
+++ Uber/screens/HomeScreen.js
@@ -6,7 +6,6 @@ import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplet
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from 'react-redux';
 import { setDestination, setOrigin } from '../slices/navSlice';
-
 const HomeScreen = () => {
     const dispatch = useDispatch();
 
@@ -44,7 +43,7 @@ const HomeScreen = () => {
                         dispatch(
                             setOrigin({
                                 location: details.geometry.location,
-                                description: data.description,
+                                description: data.description
                             })
                         );
                         dispatch(setDestination(null));
